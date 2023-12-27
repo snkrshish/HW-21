60fd72e06fd8a44ba152f798e56cb9cd9a954aab
Nikita Shilyaev <92994263+snkrshish@users.noreply.github.com>
2023-12-27 13:03:51 +0800

    create .gitignore and start setting project

---
+++ HW-21/HW-21/Sources/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  HW-21
+//
+//  Created by Никита Шиляев on 27.12.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ HW-21/HW-21/Sources/Controller/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  HW-21
+//
+//  Created by Никита Шиляев on 27.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: - Outles
+
+
+
+
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Marvel Superheroes"
+    }
+
+
+
+    //MARK: - Setup
+    private func setupHierarchy() {
+
+    }
+
+    private func setupLayout() {
+
+    }
+}
+
