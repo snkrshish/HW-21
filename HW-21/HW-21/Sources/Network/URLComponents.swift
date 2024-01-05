bc40a01c68e29094c73498a5d443187c2dd0461c
Nikita Shilyaev <92994263+snkrshish@users.noreply.github.com>
2024-01-05 13:19:20 +0800

    setup and import alamofire

---
+++ HW-21/HW-21/Sources/Network/URLComponents.swift
@@ -0,0 +1,8 @@
+//
+//  URLComponents.swift
+//  HW-21
+//
+//  Created by Никита Шиляев on 05.01.2024.
+//
+
+import Foundation
---
+++ HW-21/Pods/Target Support Files/Pods-HW-21/Pods-HW-21-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HW_21 : NSObject
+@end
+@implementation PodsDummy_Pods_HW_21
+@end
