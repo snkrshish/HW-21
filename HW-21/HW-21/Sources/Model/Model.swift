c11ef70eec1bd26e15977650460ad6984ceea2e3
Nikita Shilyaev <92994263+snkrshish@users.noreply.github.com>
2024-01-13 09:32:15 +0800

    createNetwork

--- HW-21/HW-21/Sources/Model/Model.swift
+++ HW-21/HW-21/Sources/Model/Model.swift
@@ -1,14 +1,47 @@
-struct Characters: Decodable {
-    let characters: [Character]
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let welcome = try? JSONDecoder().decode(Welcome.self, from: jsonData)
+
+import Foundation
+struct Results: Codable {
+    let results: [FinalResult]?
 }
 
-struct Character: Decodable {
-    let name: String?
-    let description: String?
-    let thumbnail: Thumbnail?
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let code: Int
+    let status, copyright, attributionText, attributionHTML: String
+    let etag: String
+    let data: DataClass
 }
 
-struct Thumbnail: Decodable {
+// MARK: - DataClass
+struct DataClass: Codable {
+    let offset, limit, total, count: Int
+    let results: [FinalResult]?
+}
+
+// MARK: - Result
+struct FinalResult: Codable {
+    let name, description: String?
+    let modified: String
+    let thumbnail: Thumbnail
+}
+
+// MARK: - Thumbnail
+struct Thumbnail: Codable {
     let path: String
-    let extensions: String
+    let thumbnailExtension: Extension
+
+    enum CodingKeys: String, CodingKey {
+        case path
+        case thumbnailExtension = "extension"
+    }
+}
+
+enum Extension: String, Codable {
+    case gif = "gif"
+    case jpg = "jpg"
 }
